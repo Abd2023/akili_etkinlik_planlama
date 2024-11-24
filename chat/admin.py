b5bd3ab54cb03644d5a0e6f36ca4d7d47b6f9338
Riyaz <rijazrijaz5555@gmail.com>
2024-11-24 16:46:48 +0300

    temiz

--- chat/admin.py
+++ chat/admin.py
@@ -3,11 +3,3 @@ from .models import Message
 
 admin.site.register(Message)
 
-# Register your models here.
-#yeni bir sey yazdim
-##
-# djhflkdhflkad#
-#adddddddd
-# super man dklhflksdflksjfkldsjflksjflkjsflksdf
-# dfhsdlfkhsklfhsd
-# sfkhsdfkhskfs #
