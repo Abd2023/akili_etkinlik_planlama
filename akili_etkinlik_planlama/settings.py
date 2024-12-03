718fdd08cfe74bb69670f39b2dcdde07bc027cd0
Abdullah Amin <abdullahamin2023@gmail.com>
2024-12-03 16:31:35 +0300

    new changes made

--- akili_etkinlik_planlama/settings.py
+++ akili_etkinlik_planlama/settings.py
@@ -16,7 +16,6 @@ import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -26,13 +25,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "FALSE").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(" ")
 
 # Application definition
 
 INSTALLED_APPS = [
-    #'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -43,42 +41,26 @@ INSTALLED_APPS = [
     'chat',
     'recommendation',
     'admin_panel',
-    
 ]
 
 AUTH_USER_MODEL = 'user_management.CustomUser'
 
-LOGIN_URL = '/login/' 
- 
+LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/home/'
+LOGOUT_REDIRECT_URL = '/login/'
 
-LOGOUT_REDIRECT_URL = '/login/'  
-
-SESSION_EXPIRE_AT_BROWSER_CLOSE = True  
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
-
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.cache.UpdateCacheMiddleware',
-    'django.middleware.cache.FetchFromCacheMiddleware',
-    
 ]
-
-CACHE_MIDDLEWARE_SECONDS = 0
-CACHE_MIDDLEWARE_KEY_PREFIX = ''
-
 
 ROOT_URLCONF = 'akili_etkinlik_planlama.urls'
 
@@ -98,101 +80,48 @@ TEMPLATES = [
     },
 ]
 
-# settings.py
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-EMAIL_HOST = 'smtp.gmail.com'  # For Gmail
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False  # Ensure this is False, as you're using TLS
-EMAIL_HOST_USER = 'testsifreunuttum@gmail.com'
-EMAIL_HOST_PASSWORD = 'xirn fvkl vcic yujt'  # Your email password
-
-DEFAULT_FROM_EMAIL = 'Akili Etkinlik Planlama Sistemi <noreply@yourdomain.com>'
-
-
-import logging
-logging.basicConfig(level=logging.DEBUG)
-
-
-
 WSGI_APPLICATION = 'akili_etkinlik_planlama.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 database_url = os.environ.get("DATABASE_URL")
 
+print(f"DATABASE_URL: {database_url}")
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(database_url)
 }
 
-
-DATABASES["default"] = dj_database_url.parse(database_url)
-
-#postgresql://akili_etkinlik_planalama_user:0kokY6AEjOcAOKEl5jRzz9Jhv6oRUAAm@dpg-ct6srcalqhvc73at4sj0-a.oregon-postgres.render.com/akili_etkinlik_planalama
-
-
-
 # Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
-#AUTH_PASSWORD_VALIDATORS = [
-#    {
-#        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-#    },
-#    {
-#        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-#    },
-#    {
-#        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-#    },
-#    {
-#        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-#    },
-#]
-
-# Or, to allow simple passwords, use an empty list
 AUTH_PASSWORD_VALIDATORS = []
 
 # Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = '/static/'
 
 # The directory where `collectstatic` will store the static files
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Directories where Django will search for additional static files
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',  # You can add more directories if needed
+    os.path.join(BASE_DIR, 'static'),
 ]
+
+# Use Whitenoise to compress and serve static files efficiently
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (user-uploaded content)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
@@ -200,10 +129,11 @@ AUTHENTICATION_BACKENDS = [
 
 GOOGLE_MAPS_API_KEY = "AlzaSy-tHO5caZi4LhDsnwUJUOGvOda7MwWdUPi"
 
-
-
-
-SESSION_ENGINE = "django.contrib.sessions.backends.db"  # Default session backend
-CACHE_BACKEND = 'django.core.cache.backends.locmem.LocMemCache'  # Local memory cache
-
-
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'testsifreunuttum@gmail.com'
+EMAIL_HOST_PASSWORD = 'xirn fvkl vcic yujt'
+DEFAULT_FROM_EMAIL = 'Akili Etkinlik Planlama Sistemi <noreply@yourdomain.com>'
